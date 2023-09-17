b0e1e639011505171f62a10b4a5d860eb4b625e1
C-Berggren <>
2023-09-17 12:12:27 +0000

    added borders and starting up javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const tilesContainer = document.querySelector("cards");
+const animals = ["bee", "butterfly", "cat", "cow", "deer", "dog", "elephant", "giraffe", "horse", "lamb", "lion", "owl", "raccoon", "tiger", "turtle", "wolf"];
+
+animalsPicklist[...animals ...animals];
+console.log(animalPicklist);
