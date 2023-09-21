6a586a54eca0001945753c7bc2c26c1baef1d99d
C-Berggren <>
2023-09-21 12:39:51 +0000

    adding front-face class to images

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const memoryCards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    console.log('I was clicked')
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        console.log({hasFlippedCard, firstCard});
+    }
 }
 
 memoryCards.forEach(card => card.addEventListener('click', flipCard))
